86aabfc2d3ca483bc6d4452b8b93f2d8542628d8
Mauricio Oliveira <ciumauri@gmail.com>
2024-04-02 19:32:42 -0300

    Enviando arquivos finalizados

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.eventsapp;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.eventsapp.databinding.ActivityMainBinding;
 
@@ -17,12 +22,66 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonShowText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyFormatting();
+            }
         });
+
+        binding.rdgCor.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                applyFormatting();
+            }
+        });
+    }
+
+    private void applyFormatting() {
+        String text = binding.editText.getText().toString();
+
+        if (!TextUtils.isEmpty(text)) {
+            // Aplicar tamanho da fonte
+            int textSize = binding.skbTextSize.getProgress();
+            binding.editText.setTextSize(textSize);
+
+            // Aplicar estilo
+            int fontStyle = Typeface.NORMAL;
+            if (binding.chkNegrito.isChecked()) {
+                fontStyle |= Typeface.BOLD;
+            }
+            if (binding.chkItalico.isChecked()) {
+                fontStyle |= Typeface.ITALIC;
+            }
+            binding.editText.setTypeface(null, fontStyle);
+
+            // Aplicar maiúsculas
+            if (binding.chkMaiuscula.isChecked()) {
+                text = text.toUpperCase();
+            }
+
+            // Aplicar cor
+            int color = getColorSelection();
+            binding.editText.setTextColor(color);
+        }
+    }
+
+    private int getColorSelection() {
+        int color;
+        int checkedRadioButtonId = binding.rdgCor.getCheckedRadioButtonId();
+        if (checkedRadioButtonId == R.id.rdbVermelho) {
+            color = ContextCompat.getColor(this, R.color.vermelho);
+        } else if (checkedRadioButtonId == R.id.rdbVerde) {
+            color = ContextCompat.getColor(this, R.color.verde);
+        } else if (checkedRadioButtonId == R.id.rdbAzul) {
+            color = ContextCompat.getColor(this, R.color.azul);
+        } else {
+            color = Color.BLACK;
+        }
+        return color;
     }
 }
